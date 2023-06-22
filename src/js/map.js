c18766a7665488e6f81327cffcf587966e464bec
Visal Sork <visalsork@Visals-Macbook.local>
2023-06-23 00:26:30 +0700

    map

---
+++ src/js/map.js
@@ -0,0 +1,3 @@
+const apiKey = 'pk.eyJ1Ijoic2FsYm95bG95IiwiYSI6ImNsajdlMGZhejBxZW0zc29yeGRhc2wyejEifQ.RnDTZpaUp4Z71AgL3NF60w';
+
+const map = L.map('map').setView([51.505, -0.09], 13);
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,21 +1,26 @@
 <template>
   <h1>Hello Map</h1>
-  <GoogleMap api-key="AIzaSyAU_BotDdo0o7O6a1dOMv6O_7UxFfgOuzs" style="width: 100%; height: 500px" :center="center" :zoom="15">
+  <div id="map"></div>
+  <!-- <GoogleMap api-key="AIzaSyAU_BotDdo0o7O6a1dOMv6O_7UxFfgOuzs" style="width: 100%; height: 500px" :center="center" :zoom="15">
     <Marker :options="{ position: center }" />
-  </GoogleMap>
+  </GoogleMap> -->
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { GoogleMap, Marker } from "vue3-google-map";
+// import { ref } from 'vue';
+// import { GoogleMap, Marker } from "vue3-google-map";
 
-const center =ref({
-    lat: 40.689247, 
-    lng: -74.044502
-})
+// const center =ref({
+//     lat: 40.689247, 
+//     lng: -74.044502
+// })
+
 
 </script>
 
-<style>
-
+<style scoped>
+#map{
+  height: 200px;
+  width: 800px;
+}
 </style>
